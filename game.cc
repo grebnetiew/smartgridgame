3c102950825d58f07e1d4119454f110d1b1eb642
Erik Weitenberg <e.r.a.weitenberg@rug.nl>
2015-09-11 18:27:19 +0200

    leds and lcd programmed

--- game.cc
+++ game.cc
@@ -33,15 +33,32 @@ void loop() {
     if(++minutes == 60) {
         minutes = 0;
         state.tick();
-        updateScores();
-        updateLEDs();
+        board.setLeds(state);
     }
+    setLCD();
 }
 
 void updateScores() {
-
+    state.d_score += 
+        (board.ledCity[0] == 2 ? 100 : (board.ledCity[0] == 1 || board.ledCity[0] == 3) ? 20 : 0) +
+        (board.ledCity[1] == 2 ? 100 : (board.ledCity[1] == 1 || board.ledCity[1] == 3) ? 20 : 0) +
+        (board.ledCity[2] == 2 ? 100 : (board.ledCity[2] == 1 || board.ledCity[2] == 3) ? 20 : 0) +
+        (board.ledCity[3] == 2 ? 100 : (board.ledCity[3] == 1 || board.ledCity[3] == 3) ? 20 : 0) +
+        600 - board.ledCoal*300;
 }
 
-void updateLEDs() {
+void setLCD() {
+    updateScores();
+    lcd.setCursor(0, 0);
+    lcd.print("Tijd:    :");
+    lcd.setCursor(0, 1);
+    lcd.print("Prijs:");
 
+    lcd.setCursor(7, 0);
+    lcd.print(state.d_time / 10);
+    lcd.setCursor(10, 0);
+    lcd.print(state.d_time * 6 + minutes / 10); // Does it make sense? No. Don't care though :)
+
+    lcd.setCursor(7, 1);
+    lcd.print(state.d_price);
 }
