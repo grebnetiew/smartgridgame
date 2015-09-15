b69d9698a6cb6e4f1c2709a22e4a350e7b9be5f0
Erik Weitenberg <e.r.a.weitenberg@rug.nl>
2015-09-15 14:13:20 +0200

    it now actually works on arduiono

--- game/game.ino
+++ game/game.ino
@@ -1,45 +1,56 @@
+#include <Wire.h>
+
 // Made for use with Arduino
 // todo: buttons
 
-#include <Wire.h>
-#include <Adafruit_MCP23017.h>
 #include <Adafruit_RGBLCDShield.h>
 #include "c:\Users\p250644\Documents\PhD\etc\festival-grrn\game\expander.ih"
 
 using namespace std;
 
-void log(String message) {
-  // Nothing happens
+void log(String message) {}
+
+String doubleDigit(int i) {
+  static String zero('0');
+  return i > 9 ? String(i) : String(0) + String(i);
 }
 
 #include "c:\Users\p250644\Documents\PhD\etc\festival-grrn\game\state.ih"
 
-Adafruit_RGBLCDShield lcd = Adafruit_RGBLCDShield();
-IOExpander expander       = IOExpander();
+class Vars {
+  public:
+    Adafruit_RGBLCDShield lcd;
+    IOExpander expander;
+    CityState state;
+    BoardState board;
+    Vars() : lcd(), expander(), state(), board(expander) {}
+};
 
-CityState state;
-BoardState board(expander);
+Vars *v;
+
 size_t minutes = 0;
 
 void setup() {
+  v = new Vars();
   // Arduino setup phase
   pinMode(0, INPUT);
   Serial.begin(9600);
-  lcd.begin(16, 2); // This contains Wire.begin()
+  v->lcd.begin(16, 2); // This contains Wire.begin()
 }
 
 void loop() {
-  if(++minutes == 60) {
+  if(++minutes == 12) {
     minutes = 0;
-    state.tick();
-    board.setLeds(state);
+    v->state.tick();
+    v->board.setLeds(v->state);
   }
-  board.readButtons(state);
+  // v->board.readButtons(v->state);
   setLCD();
 }
 
 void updateScores() {
-  state.d_score += 600 - board.ledCoal*300 +
+  BoardState &board(v->board);
+  v->state.d_score += 600 - board.ledCoal*300 +
     (board.ledCity[0] == 2 ? 100 : (board.ledCity[0] == 1 || board.ledCity[0] == 3) ? 20 : 0) +
     (board.ledCity[1] == 2 ? 100 : (board.ledCity[1] == 1 || board.ledCity[1] == 3) ? 20 : 0) +
     (board.ledCity[2] == 2 ? 100 : (board.ledCity[2] == 1 || board.ledCity[2] == 3) ? 20 : 0) +
@@ -48,17 +59,18 @@ void updateScores() {
 
 void setLCD() {
   updateScores();
-  lcd.setCursor(0, 0);
-  lcd.print("Tijd:    :");
-  lcd.setCursor(0, 1);
-  lcd.print("Prijs:");
+  v->lcd.setCursor(0, 0);
+  v->lcd.print("Tijd:");
+  v->lcd.setCursor(0, 1);
+  v->lcd.print("Prijs:");
 
-  lcd.setCursor(7, 0);
-  lcd.print(state.d_time / 10);
-  lcd.setCursor(10, 0);
-  lcd.print(state.d_time * 6 + minutes / 10); // Does it make sense? No. Don't care though :)
+  v->lcd.setCursor(7, 0);
+  v->lcd.print(doubleDigit(v->state.d_time / 10));
+  v->lcd.setCursor(9, 0);
+  v->lcd.print(":");
+  v->lcd.print(doubleDigit(v->state.d_time % 10 * 6 + minutes / 2)); // Does it make sense? No. Don't care though :)
 
-  lcd.setCursor(7, 1);
-  lcd.print(state.d_price);
+  v->lcd.setCursor(7, 1);
+  v->lcd.print(v->state.d_price);
 }
 
