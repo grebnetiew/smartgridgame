ffa28328efd124387c717dafd64a6447e48f90f6
Erik Weitenberg <e.r.a.weitenberg@rug.nl>
2015-09-22 13:53:27 +0200

    haat

---
+++ search-expander/search-expander.ino
@@ -0,0 +1,45 @@
+#include <Wire.h>
+
+#include <Adafruit_RGBLCDShield.h>
+int ar(int pin){
+  return analogRead(pin);
+}
+void log(String message);
+#include "c:\Users\p250644\Documents\PhD\etc\festival-grrn\game\expander.ih"
+
+using namespace std;
+
+
+String nDigit(int i, size_t digits, char pad = '0') {
+  if (digits == 1) {
+    return String(i);
+  }
+  return i >= pow(10, digits - 1) ? String(i) : String(pad) + nDigit(i, digits - 1, pad);
+}
+
+
+String doubleDigit(int i) {
+  return nDigit(i, 2);
+}
+
+void log(String message) {
+  static Adafruit_RGBLCDShield lcd;
+  lcd.begin(16, 2); // This contains Wire.begin()
+  lcd.setCursor(0, 0);
+  lcd.print(message + String(" "));
+  delay(1000);
+}
+
+void setup() {
+  Serial.begin(9600);
+}
+bool tobias = false;
+void loop() {
+  IOExpander expa(0x21, 0x22);
+  for(size_t i = 0; i != 5; ++i) {
+    for(size_t j = 0; j != 28; ++j) {
+      expa.set(j, i == j);
+    }
+    delay(1000);
+  }
+}
